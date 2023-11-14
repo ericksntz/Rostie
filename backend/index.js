510a1183d03f71e6dfc181fbf9bc6db82b92785c
Humberto da Torre Murad <humbertomurad@uol.com.br>
2023-11-14 17:25:37 -0300

    [UPDATE] Validacao de cadastro e login

--- backend/index.js
+++ backend/index.js
@@ -13,21 +13,6 @@ const frontendPath = path.join(process.cwd(), "../frontend");
 
 app.use("/user", rotasUsuario);
 
-// Configurar para servir arquivos estáticos do diretório 'style'
-app.use(express.static(path.join(frontendPath, "style")));
-
-app.get("/", (req, res) => {
-    // Caminho completo para o arquivo 'home.html'
-    const filePath = path.join(frontendPath, "pages", "home.html");
-
-    // Verificar se o arquivo existe antes de tentar enviá-lo
-    if (fs.existsSync(filePath)) {
-        res.sendFile(filePath);
-    } else {
-        res.status(404).send("Arquivo não encontrado");
-    }
-});
-
 app.listen(5752, () => {
     console.log("Servidor rodando na porta 5752");
 });
